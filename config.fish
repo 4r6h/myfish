6524d4184faa5f4cffae7a89b80ef2c73ca6dc0b
4r6h <hrahat023@gmail.com>
2022-08-04 08:15:09 +0000

    updated fish aliases on a seprate file

--- config.fish
+++ config.fish
@@ -1,55 +1,34 @@
+# Add bash aliases.
+
+if [ -f ~/.aliases_4r6h ]; then
+    source ~/.aliases_4r6h
+fi
+
 if status is-interactive
     # Run "neofetch" for System info everytime fish is opened
     # neofetch
     
     # Run "pfetch" for System info everytime fish is opened
-    pfetch
+    # pfetch
     
     # Adding the Path for ST-Development Folder
-    set -gx fish_user_paths ~/.local/bin/ ~/.local/share/nvim/lsp_servers/python/node_modules/.bin ~/.local/share/nvim/lsp_servers/rust ~/.cargo/bin ~/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin 
-    set -gx LD_LIBRARY_PATH /usr/local/lib
+
+    #set -gx fish_user_paths ~/.local/bin/ ~/.local/share/nvim/lsp_servers/python/node_modules/.bin ~/.local/share/nvim/lsp_servers/rust ~/.cargo/bin ~/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin 
     
+    #set -gx LD_LIBRARY_PATH /usr/local/lib
+    
+
     # Adding the Path for ST-Development Folder in the bashrc format
+
     #export PATH=$PATH:$HOME/ST-Development/stlink-1.7.0/
-    
 
-#################################################
-       ####ALIASES ADDED BY 4R6H/RAHAT####
-
-###For PACMAN###
-alias pacman="sudo pacman"
-
-###For BASH###
-alias vbc="vim $HOME/.bashrc"
-alias sbrc="source $HOME/.bashrc"
-
-###For FISH###
-alias vfc="vim $HOME/.config/fish/config.fish"
-alias sfrc="source $HOME/.config/fish/config.fish"
-
-###For ALACRITTY###
-alias val="vim $HOME/.config/alacritty/alacritty.yml"
-
-###For SDDM SUGAR CANDY THEME###
-alias vsc="sudo vim /usr/share/sddm/themes/Sugar-Candy/theme.conf"
-
-###For COLORSCRIPT###
-alias cs="colorscript"
-alias csr="colorscript random"
-colorscript -e awk-rgb-test
 
 echo "WELLCOME TO RAHAT'S FISH CONIG"
 
-#################################################   
-    
-    # Adding Aliases
-    # alias ls="ls -lah --color=auto"
-    alias ll="lsd -lah"
-    alias cc="clear"
-    
     #set -U fish_color_param blue
-    set fish_greeting
+    #set fish_greeting
 
     # Starting the starship prompt
-    #starship init fish | source
+
+      starship init fish | source
 end
